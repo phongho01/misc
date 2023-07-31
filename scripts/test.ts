0ee18eb3270470446ad570a4ec3e6edacb1aea50
phongho01 <phong.ho@napaglobal.com>
2023-07-31 11:28:53 +0700

    test send tx

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,15 +1,31 @@
-import { ethers } from 'ethers';
-import ERC20_ABI from './ABI.json';
+require('dotenv').config();
+import { ethers } from 'hardhat';
 
-const CONTRACT_ADDRESS = '0x1ffB5A1D759C67084ea583C99891b1be34d323d3';
-const RPC_URL = 'wss://rpc-kura.cross.technology';
+async function main() {
+  console.log('========== STARTING ==========');
+  const [deployer, ...accounts] = await ethers.getSigners();
 
-const provider = new ethers.providers.WebSocketProvider(RPC_URL);
+  const nonce = await deployer.getTransactionCount();
+  console.log('nonce', nonce);
 
-const contract = new ethers.Contract(CONTRACT_ADDRESS, ERC20_ABI, provider);
+  const txData = {
+    to: '0x4F9EF07A6DDF73494D2fF51A8f7B78e9c5815eb2',
+    value: ethers.utils.parseEther('0.001'),
+    nonce: nonce,
+    gasPrice: 1000000000,
+  };
 
-contract.on('Transfer', (from, to, value, event) => {
-  console.log('event: ', event);
+  const tx = await deployer.sendTransaction(txData);
+  console.log(tx.hash, tx.gasPrice, tx.gasLimit);
+
+  // txData.gasLimit = 42000;
+  // const tx2 = await deployer.sendTransaction(txData);
+  // console.log(tx2);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
 });
-
-// mint();
